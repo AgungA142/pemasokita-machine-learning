6c66679366b7a02abb19e1c936df778a1c93ff55
Surya <suryanurjaman91@gmail.com>
2023-06-15 17:15:24 +0700

    final

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,20 @@
+# get python image
 FROM python:3.10.10
 
-WORKDIR /workspace
-
-ENV PIP_ROOT_USER_ACTION=ignore
+# copy necessary files for the container
+COPY requirements.txt .
+COPY main.py .
 
 RUN pip install --upgrade pip
 
-COPY requirements.txt requirements.txt
-
-RUN pip install -r requirements.txt
-
-# Install OpenCV
 RUN apt-get update && \
     apt-get install -y libgl1-mesa-dev && \
     pip install opencv-python
+# install dependencies
+RUN pip install -r requirements.txt
 
-COPY . .
-
+# expose port
 EXPOSE 8080
 
-ENV PYTHONUNBUFFERED=1
-
-# CMD ["python", "main.py"]
-
-CMD ["uvicorn", "--host", "0.0.0.0", "--port", "8080", "main:app"]
+# finally run the application inside the container 
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
--- main.py
+++ main.py
@@ -1,64 +1,58 @@
-from enum import Enum
-from fastapi import FastAPI, File, UploadFile
+import uvicorn
+from fastapi import FastAPI, Response, UploadFile
 import cv2
 import tensorflow as tf
+from PIL import Image
+from io import BytesIO
 import numpy as np
-import matplotlib.pyplot as plt
-import uvicorn
-import os
+from enum import Enum
 
-app = FastAPI()  # Create a new FastAPI app instance
-
-# Define an Enum class to represent the possible categories for an item
-
+# initiate an app
+app = FastAPI()
+new_model = tf.keras.models.load_model('bean_condition_saved_model')
+# create a greeting message for an endpoint '/'
+# we use neither path nor query parameters in this endpoint
 
 class BeanQuality(str, Enum):
     good = "good"
     bad = "bad"
 
+def preprocess_image(image):
+    img = cv2.resize(image, (224, 224))
+    img = img / 255.0
+    return img
 
-# Load the pre-trained model
-new_model = tf.keras.models.load_model('./bean_condition_saved_model')
+def predict_condition(image):
+    preprocessed_image = preprocess_image(image)
+    predictions = new_model.predict(np.expand_dims(preprocessed_image, axis=0))
+    predicted_label = int(round(predictions[0][0]))
 
-# Define the predict endpoint
+    print(f"{'bad' if predicted_label == 0 else 'good'} bean.")
+
+    if predicted_label == 0:
+        return BeanQuality.bad
+    else:
+        return BeanQuality.good
 
 
-@app.post("/predict", response_model=BeanQuality)
-async def predict(file: UploadFile = File(..., description="Image file to predict")):
-    def preprocess_image(image):
-        img = cv2.resize(image, (224, 224))
-        img = img / 255.0
-        return img
+@app.get('/')
+async def greeting():
+    return 'Hello World!'
 
-    def predict_image(image):
-        predictions = new_model.predict(np.expand_dims(image, axis=0))
-        predicted_label = int(round(predictions[0][0]))
+@app.post("/predict_condition", response_model=BeanQuality)
+async def predict_condition_endpoint(uploaded_file: UploadFile, response: Response):
+    if uploaded_file.content_type not in ["image/jpeg", "image/png"]:
+        response.status_code = 400
+        return "File is Not an Image"
 
-        # Print the predicted label
-        print(f"{'bad' if predicted_label == 0 else 'good'} bean.")
-
-        # Load and display the image
-        plt.imshow(image)
-        plt.title(
-            f"Predicted: {'bad' if predicted_label == 0 else 'good'} bean")
-        plt.axis('off')
-        plt.show()
-
-        if predicted_label == 0:
-            return BeanQuality.bad
-        else:
-            return BeanQuality.good
-
-    # Read the image file
-    img = cv2.imdecode(np.fromstring(await file.read(), np.uint8), cv2.IMREAD_COLOR)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    # Preprocess and predict the image
-    img = preprocess_image(img)
-    result = predict_image(img)
+    image = np.array(Image.open(BytesIO(uploaded_file.file.read())))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    result = predict_condition(image)
 
     return result
 
-port = os.environ.get("PORT", 8080)
-print(f"Listening to http://0.0.0.0:{port}")
-uvicorn.run(app, host='0.0.0.0',port=port)
+
+  
+# run the app on defined host and port
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8080)

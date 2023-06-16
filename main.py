0724ceddb1de07b60808965f198df11682c4a58a
sacred142 <agung.alfatah43@gmail.com>
2023-06-16 17:58:26 +0700

    adding form in html files

--- main.py
+++ main.py
@@ -1,41 +1,61 @@
-import uvicorn
+from enum import Enum
 from fastapi import FastAPI, Response, UploadFile
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse, JSONResponse
 import cv2
 import tensorflow as tf
 from PIL import Image
 from io import BytesIO
 import numpy as np
-from enum import Enum
-from google.cloud import storage
-import os
+import uvicorn
 
-# initiate an app
-app = FastAPI()
+app = FastAPI(debug=True)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-storage_client = storage.Client.from_service_account_json('./key.json')
-bucket = storage_client.bucket('model-bucket-pmskt')
-blob = bucket.blob('best_model_coba3.h5')
-
-destination_filename = 'best_model_coba3.h5'
-
-if not os.path.exists(destination_filename):
-    blob.download_to_filename(destination_filename)
-    print('File downloaded.')
-else:
-    print('File already exists.')
-
-new_model = tf.keras.models.load_model('./best_model_coba3.h5')
-# create a greeting message for an endpoint '/'
-# we use neither path nor query parameters in this endpoint
 
 class BeanQuality(str, Enum):
     good = "good"
     bad = "bad"
 
+
+class BeanRoast(str, Enum):
+    Raw = "Raw"
+    MediumRoast = "Medium Roast"
+    DarkRoast = "Dark Roast"
+    LightRoast = "Light Roast"
+
+
+class BeanType(str, Enum):
+    Arabika = "Arabika"
+    Robusta = "Robusta"
+    liberica = "liberica"
+
+
 def preprocess_image(image):
     img = cv2.resize(image, (224, 224))
     img = img / 255.0
     return img
+
+
+def predict_roast(image):
+    preprocessed_image = preprocess_image(image)
+    predictions = new_model2.predict(
+        np.expand_dims(preprocessed_image, axis=0))
+    predicted_label = np.argmax(predictions)
+
+    class_names = ['Raw', 'Medium Roast', 'Dark Roast', 'Light Roast']
+    predicted_class = class_names[predicted_label]
+    print(f"{predicted_class} bean.")
+
+    if predicted_label == 0:
+        return BeanRoast.Raw
+    elif predicted_label == 1:
+        return BeanRoast.MediumRoast
+    elif predicted_label == 2:
+        return BeanRoast.DarkRoast
+    else:
+        return BeanRoast.LightRoast
+
 
 def predict_condition(image):
     preprocessed_image = preprocess_image(image)
@@ -50,9 +70,28 @@ def predict_condition(image):
         return BeanQuality.good
 
 
-@app.get('/')
-async def greeting():
-    return 'Hello World!'
+def predict_type(image):
+    preprocessed_image = preprocess_image(image)
+    predictions = new_model3.predict(
+        np.expand_dims(preprocessed_image, axis=0))
+    predicted_label = np.argmax(predictions)
+
+    class_names = ['Arabika', 'Robusta', 'liberica']
+    predicted_class = class_names[predicted_label]
+    print(f"{predicted_class} bean.")
+
+    if predicted_label == 0:
+        return BeanType.Arabika
+    elif predicted_label == 1:
+        return BeanType.Robusta
+    else:
+        return BeanType.liberica
+
+
+new_model = tf.keras.models.load_model('model_bean_condition.h5')
+new_model2 = tf.keras.models.load_model('crop_classification_model-roast.h5')
+new_model3 = tf.keras.models.load_model('model-arabika.h5')
+
 
 @app.post("/predict_condition", response_model=BeanQuality)
 async def predict_condition_endpoint(uploaded_file: UploadFile, response: Response):
@@ -64,10 +103,35 @@ async def predict_condition_endpoint(uploaded_file: UploadFile, response: Respon
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     result = predict_condition(image)
 
-    return result
+    return JSONResponse({"result": result})
 
 
-  
-# run the app on defined host and port
-if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+@app.post("/predict_roast", response_model=BeanRoast)
+async def predict_roast_endpoint(uploaded_file: UploadFile, response: Response):
+    if uploaded_file.content_type not in ["image/jpeg", "image/png"]:
+        response.status_code = 400
+        return "File is Not an Image"
+
+    image = np.array(Image.open(BytesIO(uploaded_file.file.read())))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    result = predict_roast(image)
+
+    return JSONResponse({"result": result})
+
+
+@app.post("/predict_type", response_model=BeanType)
+async def predict_type_endpoint(uploaded_file: UploadFile, response: Response):
+    if uploaded_file.content_type not in ["image/jpeg", "image/png"]:
+        response.status_code = 400
+        return "File is Not an Image"
+
+    image = np.array(Image.open(BytesIO(uploaded_file.file.read())))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    result = predict_type(image)
+
+    return JSONResponse({"result": result})
+
+
+@app.get("/upload")
+async def get_html():
+    return FileResponse("static/index.html", media_type="text/html")
